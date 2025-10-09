d00a6eba1ca7749b0c7925c7a4fe6aa4cad54b93
Jaryl <jvbaroro@n-pax.com>
2025-10-09 08:06:50 +0800

    gitignore missed files

--- MediaAPI/obj/Debug/net9.0/MediaAPI.AssemblyInfo.cs
+++ MediaAPI/obj/Debug/net9.0/MediaAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MediaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4f57c5b4caaefb731926225fd5271a83f6df526")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+518f93edf233bcb14d0d27d2bba80201da26b6e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MediaAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MediaAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
